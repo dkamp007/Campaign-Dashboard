9882e76bce8ad0d5d5680a6e1216008f08ac0532
dkamp007 <158074291+dkamp007@users.noreply.github.com>
2025-07-09 13:30:20 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -4,13 +4,15 @@ import streamlit as st
 import toml
 
 @st.cache_resource
-def get_connection():
-    config = toml.load("config.toml")["database"]
-    engine = sqlalchemy.create_engine(
-        f"mysql+mysqlconnector://{config['user']}:{config['password']}@{config['host']}/{config['database']}"
-    )
-    return engine
-
 def run_query(query):
-    engine = get_connection()
-    return pd.read_sql(query, engine)
+    config = toml.load("config.toml")["database"]
+    engine = None
+    try:
+        engine = sqlalchemy.create_engine
+        (
+            f"mysql+mysqlconnector://{config['user']}:{config['password']}@{config['host']}/{config['database']}"
+        )
+        df = pd.read_sql(query, engine)
+        return df
+    except Exception as e:
+        print(f"An error occurred: {e}")
